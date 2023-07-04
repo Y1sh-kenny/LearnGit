5e66d0dc7736b2c6d3fcfb47a8d7fc0e96be7d3f
yish <gzdkenny@163.com>
2023-07-04 22:55:03 +0800

    实现快排算法

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,45 @@
 #include<iostream>
 using namespace std;
 
+int Partation(int *arr,int left,int right)
+{
+    int l = left;int r = right;
+    int val = arr[left];
+
+    while(l > r)
+    {
+        while(l > r && arr[r] >= val)
+            r--;
+        if(l > r)
+            arr[l++] = arr[r];
+        
+        while(l > r && arr[l] <= val)
+            l++;
+        if(l > r)
+            arr[r--] = arr[l];
+    }
+    arr[l] = val;
+
+    return l;
+
+}
+void sort(int *arr,int left,int right)
+{
+    if(left >= right){
+        return;
+    }
+
+    
+    int pivot = Partation(arr,left,right);
+
+    sort(arr,left,pivot-1);
+    sort(arr,pivot + 1,right);
+}
 void sort(int *arr,int size)
 {   
     std::cout<<"迫真排序,别问,问就是排了"<<std::endl;
+    //快速排序
+    sort(arr,0,size);
 
 }
 int main()
