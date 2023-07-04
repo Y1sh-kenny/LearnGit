fb27660615932624c0b592e5c0a5625b59f2ef60
yish <gzdkenny@163.com>
2023-07-04 14:58:18 +0800

    add a main.cpp and vscode.json

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+void sort(int *arr,int size)
+{
+
+}
+int main()
+{
+    std::cout<<"version 1.0"<<std::endl;
+    int arr[] = {12,4,89,43,21,78};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sort(arr,size);
+}
